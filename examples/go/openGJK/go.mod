4e432555fb747a7734a3308f1a7dc81a02844d69
Mattia Montanari <mattia.montanari@eng.ox.ac.uk>
2026-01-03 23:42:16 +0100

    refactor: remove duplicate files (now in scalar/)
    - Delete examples/ (moved to scalar/examples/)
    - Delete include/openGJK/ (moved to scalar/include/openGJK/)
    - Delete openGJK.c (moved to scalar/openGJK.c)

--- examples/go/openGJK/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/MattiaMontanari/openGJK/examples/go/openGJK
-
-go 1.18
--- examples/python_ctypes/src/pyopengjk/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Python wrapper for openGJK.
-
-openGJK is a fast and robust C implementation of the
-Gilbert-Johnson-Keerthi (GJK) algorithm.
-"""
-
-from .opengjk import compute_minimum_distance, Point3, Simplex, DistanceResult
-
-__all__ = ["compute_minimum_distance", "Point3", "Simplex", "DistanceResult"]
